56805e58c5655bc41427f5a118e5146df049a18e
manueeeeeeo <manu.engenios@gamil.com>
2025-01-25 00:35:04 +0100

    Cambios en AppLifecycleManager para manejar correctamente los logins, logout y los diferentes estados de vida de la app

--- app/src/main/java/com/clase/engenios_manuelimdbapp_v20/utils/AppLifecycleManager.java
+++ app/src/main/java/com/clase/engenios_manuelimdbapp_v20/utils/AppLifecycleManager.java
@@ -16,61 +16,106 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class AppLifecycleManager extends Application implements Application.ActivityLifecycleCallbacks {
 
-    private boolean isInBackground = false;
-    private int activityReferences = 0;
+    private boolean isInBackground = false;  // Variable para manejar si la app está de fondo
+    private int activityReferences = 0; // Variable para manejar los inicios de la apps
     private boolean isActivityChangingConfigurations = false;
-    private Handler logoutHandler;
-    private Runnable logoutRunnable;
-    private static final long LOGOUT_DELAY = 30000; // Ejemplo: 30 segundos
-    private SharedPreferences preferences;
-    private DatabaseUsers databaseUsers;
-    private Toast mensajeToast = null;
-    private UsersSync sincronizacionUsuarios = null;
+    // Handler y Runnable para gestionar el cierre de sesión automático tras un tiempo de inactividad
+    private Handler logoutHandler = null;
+    private Runnable logoutRunnable = null;
+    private static final long LOGOUT_DELAY = 30000; // Variable para el delay del logout
+    private SharedPreferences preferences = null; // Variable para manejar las preferencias del usuario
+    private DatabaseUsers databaseUsers = null; // Variable para la manejar la bd de los ususarios locales
+    private Toast mensajeToast = null; // Variable para manejar los Toast de está actividad
+    private UsersSync sincronizacionUsuarios = null; // Variable para manejar la sincronización de los usuario
 
-    private boolean isExplicitLogout = false; // Indicador para logout explícito
+    private boolean isExplicitLogout = false; // Variable para manejar los logout implicitos
 
     @Override
     public void onCreate() {
         super.onCreate();
         registerActivityLifecycleCallbacks(this);
 
+        // Inicializo las preferencias
         preferences = getSharedPreferences("AppPreferences", Context.MODE_PRIVATE);
-        databaseUsers = new DatabaseUsers(this); // Inicializar base de datos
+        // Incilizo la base de datos locales de ususarios
+        databaseUsers = new DatabaseUsers(this);
+        // Inicializo la base de datos en la nube
         sincronizacionUsuarios = new UsersSync();
-        logoutHandler = new Handler();
-        logoutRunnable = () -> {
-            // Acción de logout automático
-            registerUserLogout();
-        };
+        logoutHandler = new Handler(); // Inicializo el Handler de la app
+        logoutRunnable = () -> registerUserLogout(); // Llamo al método para registrar el logout del usuario
+
+        // Verifico si el usuario está logueado al inicio
+        if (preferences.getBoolean("is_logged_in", false)) { // En caso de ser así
+            // Llamo al método para registrar el login del usuario
+            registerUserLogin();
+        }
     }
 
+    /**
+     * Método en el que primero que todo obtengo el uid del usuario registrado
+     * compruebo que el uid sea bien y no este vacio, una vez comprobado, procedo
+     * a insertar el registro del logout tanto en la base de datos local y en la
+     * nube, y además en las preferencias de ususario establezco que el usuario
+     * no está logeado actualmente*/
     private void registerUserLogout() {
+        // Obtengo en una variable de tipo String el uid del usuario iniciado
         String uid = obtenerUidActual();
-        if (uid != null) {
-            databaseUsers.actualizarLogout(uid); // Registrar logout en la base de datos
+        // Compruebo si el uid es correcto y no está vacio
+        if (uid != null) { // En caso de que el uide esté bien
+            // Registro el logout en la base de datos local
+            databaseUsers.actualizarLogout(uid);
+            // Lanzo un Toast diciendo que el Logout se ha actualizado
             showToast("Logout Actualizado: "+databaseUsers.obtenerTiempoActual());
+            // Sincronizo el nuevo logout con la base de datos de Firestore
             sincronizacionUsuarios.registrarLogout(uid);
+            // Establezco en las preferencias el editor para cambiar cosas
             SharedPreferences.Editor editor = preferences.edit();
+            // En el editor establezco que el ususario no tiene la sesión iniciada
             editor.putBoolean("is_logged_in", false);
+            // Establezco los cambios en las preferencias
             editor.apply();
+            // En el LogCat imprimo que se ha registrado el logout en la bd
             Log.d("AppLifecycleManager", "Logout registrado en la base de datos.");
         }
     }
 
+    /**
+     * Método en el que primero que todo obtengo el uid del usuario registrado
+     * compruebo que el uid sea bien y no este vacio, una vez comprobado, procedo
+     * a insertar el registro del login tanto en la base de datos local y en la
+     * nube, y además en las preferencias de ususario establezco que el usuario
+     * si está logeado actualmente*/
     private void registerUserLogin() {
+        // Establezco el logout implicito como falso
         isExplicitLogout = false;
+        // Obtengo en una variable de tipo String el uid del usuario iniciado
         String uid = obtenerUidActual();
-        if (uid != null) {
-            databaseUsers.actualizarLogin(uid); // Registrar login en la base de datos
-            showToast("Login Actualizado: "+databaseUsers.obtenerTiempoActual());
+        // Compruebo si el uid es correcto y no está vacio
+        if (uid != null) { // En caso de que el uide esté bien
+            // Registro el login en la base de datos local
+            databaseUsers.actualizarLogin(uid);
+            // Registo el login en la base de datos en la nube
             sincronizacionUsuarios.registrarLogin(uid);
+            // Muestro el Toast diciendo que el login se ha actualizado
+            showToast("Login Actualizado: "+databaseUsers.obtenerTiempoActual());
+            // Establezco en las preferencias el editor para cambiar cosas
             SharedPreferences.Editor editor = preferences.edit();
+            // En el editor establezco que el ususario si tiene la sesión iniciada
             editor.putBoolean("is_logged_in", true);
+            // Establezco los cambios en las preferencias
             editor.apply();
+            // En el LogCat imprimo que se ha registrado el logout en la bd
             Log.d("AppLifecycleManager", "Login registrado en la base de datos.");
         }
     }
 
+    /**
+     * @return
+     * Método en el que lo que hago es crear una variable de tipo
+     * FirebaseUser e inicializo la instancia de la autentificación
+     * para obtener el usuario registrado, posteriormente, compruebo
+     * si el ususario está vacio, devuelvo nulo y por otro lado
+     * en caso de que si que este relleno obtengo el uid del usuario autentificado*/
     private String obtenerUidActual() {
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         return currentUser != null ? currentUser.getUid() : null;
@@ -78,66 +123,90 @@ public class AppLifecycleManager extends Application implements Application.Acti
 
     @Override
     public void onActivityResumed(Activity activity) {
+        // Establezco la variable en falso de si la app está de fondo
         isInBackground = false;
+        // Remuevo todas las callbacks que esten en ejecucción
         logoutHandler.removeCallbacks(logoutRunnable);
-
-        // Registrar el login al reanudar la actividad
-        registerUserLogin();
     }
 
     @Override
     public void onActivityPaused(Activity activity) {
+        // Establezco la variable en verdadero de si la app está de fondo
         isInBackground = true;
+        // Programo el cierre de sesión automático después de un retraso especificado
         logoutHandler.postDelayed(logoutRunnable, LOGOUT_DELAY);
     }
 
     @Override
     public void onActivityStarted(Activity activity) {
+        // En caso de que no cambien las configuraciones de la actividad
         if (!isActivityChangingConfigurations) {
+            // Inrcemento en uno el contador de referencias de actividades
             activityReferences++;
+
+            // Si es el primer inicio de actividad
+            if (activityReferences == 1 && !isExplicitLogout) {
+                // En el Log indico que la app está en primer plano
+                Log.d("AppLifecycleManager", "App entrando al primer plano, registrando login.");
+                // Llamo al método para registrar el login del usuario
+                registerUserLogin();
+            }
         }
     }
 
     @Override
     public void onActivityStopped(Activity activity) {
+        // Resto uno a la referencia de las activdidades
         activityReferences--;
+        // Establezco los cambios de las configuraciones de la actividad
         isActivityChangingConfigurations = activity.isChangingConfigurations();
 
+        // En caso de que la app haya perdido el plano principal
         if (activityReferences == 0 && !isActivityChangingConfigurations) {
+            // En caso de que no sea un logout explicito
             if (!isExplicitLogout) {
+                // Imprimo por el Log que la app ha pasado a segundo plano
                 Log.d("AppLifecycleManager", "App en segundo plano, registrando logout implícito.");
-                registerUserLogout(); // Logout implícito
+                // Llamo al método para registrar el logout
+                registerUserLogout();
             }
         }
     }
 
     @Override
     public void onActivityDestroyed(Activity activity) {
+        // Establezco los cambios de las configuraciones de la actividad
         isActivityChangingConfigurations = activity.isChangingConfigurations();
     }
 
     @Override
     public void onTrimMemory(int level) {
+        // Compruebo que el nivel de recorte de la memoria indica que la UI se ha ocutlado o pasado a segundo plano
         if (level == TRIM_MEMORY_UI_HIDDEN) {
-            // Registrar logout si la UI se minimiza
+            // Registro logout si la UI se minimiza
             registerUserLogout();
         }
         super.onTrimMemory(level);
     }
 
+    /**
+     * Método para comprobar si el Logout que
+     * ha sucedido es un Logout Explicito*/
     public void onExplicitLogout() {
+        // Señalo la variable como que si que se que es explicito
         isExplicitLogout = true;
-        registerUserLogout(); // Logout explícito
+        // Llamo al método para registrarle también
+        registerUserLogout();
     }
 
     @Override
     public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-        // Implementación vacía
+        // No utilizo esto
     }
 
     @Override
     public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
-        // Implementación vacía
+        // No utilizo esto
     }
 
     /**
