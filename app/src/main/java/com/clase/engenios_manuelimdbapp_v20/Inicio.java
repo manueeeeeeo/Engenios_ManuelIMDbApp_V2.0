96eb6e692178c3c2f84c1c435c9fa655ffc0e8f6
manueeeeeeo <manu.engenios@gamil.com>
2025-01-24 23:57:51 +0100

    Cambios para no guardar tantos datos, además de eliminar los toast molestos

--- app/src/main/java/com/clase/engenios_manuelimdbapp_v20/Inicio.java
+++ app/src/main/java/com/clase/engenios_manuelimdbapp_v20/Inicio.java
@@ -251,10 +251,10 @@ public class Inicio extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             FirebaseUser user = auth.getCurrentUser();
                             userdb.insertarUsuario(user.getUid(), user.getDisplayName(), user.getEmail());
-                            userdb.actualizarLogin(user.getUid());
-                            showToast("Login Actualizado: "+userdb.obtenerTiempoActual());
+                            //userdb.actualizarLogin(user.getUid());
+                            //showToast("Login Actualizado: "+userdb.obtenerTiempoActual());
                             sincronizarUsers.agregarOActualizarUsuario(user.getUid(), user.getDisplayName(), user.getEmail());
-                            sincronizarUsers.registrarLogin(user.getUid());
+                            //sincronizarUsers.registrarLogin(user.getUid());
                             // Inicio de sesión exitoso
                             showToast("Inicio de sesión exitoso");
                             Intent intent = new Intent(Inicio.this, MainActivity.class);
@@ -318,10 +318,10 @@ public class Inicio extends AppCompatActivity {
                                             }
 
                                             userdb.insertarUsuario(user.getUid(), user.getDisplayName(), user.getEmail());
-                                            userdb.actualizarLogin(user.getUid());
-                                            showToast("Login Actualizado: "+userdb.obtenerTiempoActual());
+                                            //userdb.actualizarLogin(user.getUid());
+                                            //showToast("Login Actualizado: "+userdb.obtenerTiempoActual());
                                             sincronizarUsers.agregarOActualizarUsuario(user.getUid(), user.getDisplayName(), user.getEmail());
-                                            sincronizarUsers.registrarLogin(user.getUid());
+                                            //sincronizarUsers.registrarLogin(user.getUid());
 
                                             Intent intent = new Intent(Inicio.this, MainActivity.class);
                                             intent.putExtra("name", user.getDisplayName());
@@ -382,10 +382,10 @@ public class Inicio extends AppCompatActivity {
                         if (task.isSuccessful()) { // En caso de que la tarea salga bien
                             FirebaseUser user = auth.getCurrentUser();
                             userdb.insertarUsuario(user.getUid(), user.getDisplayName(), user.getEmail());
-                            userdb.actualizarLogin(user.getUid());
-                            showToast("Login Actualizado: "+userdb.obtenerTiempoActual());
+                            //userdb.actualizarLogin(user.getUid());
+                            //showToast("Login Actualizado: "+userdb.obtenerTiempoActual());
                             sincronizarUsers.agregarOActualizarUsuario(user.getUid(), user.getDisplayName(), user.getEmail());
-                            sincronizarUsers.registrarLogin(user.getUid());
+                            //sincronizarUsers.registrarLogin(user.getUid());
                             // Creo un intent para poder pasar al MainAcivity una vez iniciada la sesión
                             Intent intent = new Intent(Inicio.this, MainActivity.class);
                             // Establezco como parceable la key y el valor del nombre de usuario de la cuenta que inicio
@@ -433,10 +433,10 @@ public class Inicio extends AppCompatActivity {
                 providerId = userInfo.getProviderId();
             }
 
-            userdb.actualizarLogin(currentUser.getUid());
-            showToast("Login Actualizado: "+userdb.obtenerTiempoActual());
+            //userdb.actualizarLogin(currentUser.getUid());
+            //showToast("Login Actualizado: "+userdb.obtenerTiempoActual());
             sincronizarUsers.agregarOActualizarUsuario(currentUser.getUid(), currentUser.getDisplayName(), currentUser.getEmail());
-            sincronizarUsers.registrarLogin(currentUser.getUid());
+            //sincronizarUsers.registrarLogin(currentUser.getUid());
 
             // Creo el Intent
             Intent intent = new Intent(Inicio.this, MainActivity.class);
