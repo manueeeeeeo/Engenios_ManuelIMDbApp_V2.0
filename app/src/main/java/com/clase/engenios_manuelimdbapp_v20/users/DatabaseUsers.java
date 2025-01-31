a31a605379d773ec4822df5d1abf80e9f1d2b0f8
manueeeeeeo <manu.engenios@gamil.com>
2025-01-31 10:14:05 +0100

    Cambios en las bases de datos en cuanto a métodos y unificación de las dos tablas en la misma base de datos

--- app/src/main/java/com/clase/engenios_manuelimdbapp_v20/users/DatabaseUsers.java
+++ app/src/main/java/com/clase/engenios_manuelimdbapp_v20/users/DatabaseUsers.java
@@ -17,7 +17,7 @@ import java.util.Locale;
 public class DatabaseUsers extends SQLiteOpenHelper {
     // Declaro todas las variables necesarias para está clase
     private static final String DATABASE_NAME = "users.db"; // Nombre de la base de datos
-    private static final int DATABASE_VERSION = 1; // Versión de la base de datos
+    private static final int DATABASE_VERSION = 5; // Versión de la base de datos
 
     private static final String TABLE_NAME = "usuarios"; // Establezco el nombre de la tabla donde voy a almacenar los usuarios
     private static final String COLUMN_ID = "id"; // Establezco el nombre de la columna del id del registro
@@ -47,7 +47,7 @@ public class DatabaseUsers extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         // Creo la tabla de los usuarios
-        String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" // Establezco el nombre de la tabla
+        String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" // Establezco el nombre de la tabla
                 + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " // Genero la columna del id
                 + COLUMN_UID + " TEXT NOT NULL UNIQUE, " // Genero la columna del uid
                 + COLUMN_NAME + " TEXT NOT NULL, " // Genero la columna del nombre
@@ -58,6 +58,18 @@ public class DatabaseUsers extends SQLiteOpenHelper {
                 + COLUMN_LAST_LOGIN + " TEXT, " // Genero la columna del último login
                 + COLUMN_LAST_LOGOUT + " TEXT)"; // Genero la columna del último logout
         db.execSQL(CREATE_TABLE); // Ejecuto la sentencia y creo la tabla
+
+        // Crear la tabla de películas favoritas
+        String CREATE_FAVORITES_TABLE = "CREATE TABLE IF NOT EXISTS  favorites ("
+                + "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "movie_id TEXT NOT NULL, "
+                + "image_url TEXT, "
+                + "movie_title TEXT, "
+                + "date_release TEXT, "
+                + "movie_descrip TEXT, "
+                + "movi_rating TEXT, "
+                + "user_email TEXT NOT NULL)";
+        db.execSQL(CREATE_FAVORITES_TABLE);
     }
 
     @Override
@@ -75,24 +87,47 @@ public class DatabaseUsers extends SQLiteOpenHelper {
      * el uid, nombre y email del usuario, incializo la base de datos
      * de sqlite y gracias al contentValues consigo insertar los valores
      * en un nuevo registro en la bd*/
-    public long insertarUsuario(String uid, String name, String email) {
+    public long insertarOActualizarUsuario(String uid, String name, String email) {
         // Creo el objeto de la bd local y lo incializo para así poder agregar cosas
         SQLiteDatabase db = this.getWritableDatabase();
-        // Utilizo el contentValues para para poder crear un objeto e insertarlo en la bd local
-        ContentValues values = new ContentValues();
-        // Establezco el nombre de la columna y el valor a insertar
-        values.put(COLUMN_UID, uid);
-        // Establezco el nombre de la columna y el valor a insertar
-        values.put(COLUMN_NAME, name);
-        // Establezco el nombre de la columna y el valor a insertar
-        values.put(COLUMN_EMAIL, email);
-        // Genero una variable y llamo al submétodo de insertar de la bd local y paso el contentvalues ya que el logout y el login
-        // inicialmente nulos
-        long id = db.insert(TABLE_NAME, null, values);
+
+        // Verifico si el usuario ya existe en la base de datos local basandome en el uid
+        Cursor cursor = db.query(TABLE_NAME, null, COLUMN_UID + " = ?", new String[]{uid}, null, null, null);
+
+        // Utilizo el vlor -1 para indicar que no se a actualizado ni insertado
+        long result = -1;
+
+        // Compruebo si el curso es nulo y tiene algo dentro
+        if (cursor != null && cursor.getCount() > 0) { // En caso afirmativo
+            // Significa que el usuario existe, le actualiza
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_NAME, name);
+            values.put(COLUMN_EMAIL, email);
+
+            // Actualizo el usuario con el mismo uid
+            result = db.update(TABLE_NAME, values, COLUMN_UID + " = ?", new String[]{uid});
+        } else { // En caso negativo
+            // Significa que el usuario no existe, inserto un nuevo registro
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_UID, uid);
+            values.put(COLUMN_NAME, name);
+            values.put(COLUMN_EMAIL, email);
+
+            // Inserto un nuevo usuario
+            result = db.insert(TABLE_NAME, null, values);
+        }
+
+        // Compruebo si en el cursor sigue habiendo cosas
+        if (cursor != null) { // De ser así
+            // Le cierro
+            cursor.close();
+        }
+
         // Cierro la base de datos local
         db.close();
-        // Devuelvo el valor del long que obtengo al insertar
-        return id;
+
+        // Devuelvo el resultado de la operación
+        return result;
     }
 
     /**
@@ -184,6 +219,15 @@ public class DatabaseUsers extends SQLiteOpenHelper {
         db.close();
     }
 
+    /**
+     * @param uid
+     * @param email
+     * @param numero
+     * @param name
+     * @param imagen
+     * @param ubi
+     * Método en el que le paso todos los datos que puede tener
+     * un usuario en la base de datos local y procedo a insertarlos todos*/
     public void insetarUsuarioConTodosLosDatos(String uid, String name, String email, String ubi, String numero, String imagen){
         // Creo el objeto de la bd local y lo incializo para así poder agregar cosas
         SQLiteDatabase db = this.getWritableDatabase();
